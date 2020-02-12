2ca1783c1c3454ba5c0b2d9b79f709d589413008
unknown <praveenkamate@gmail.com>
2020-02-12 08:21:38 +0530

    signup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SignUpComponent } from './Components/sign-up/sign-up.component';
+import { LoginComponent } from './Components/login/login.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : 'signup', component: SignUpComponent},
+  {path : 'login', component: LoginComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents = [SignUpComponent, LoginComponent]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
+// import { SignUpComponent } from './Components/sign-up/sign-up.component';
+// import { LoginComponent } from './Components/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    // SignUpComponent,
+    // LoginComponent
+    routingComponents
   ],
   imports: [
     BrowserModule,
