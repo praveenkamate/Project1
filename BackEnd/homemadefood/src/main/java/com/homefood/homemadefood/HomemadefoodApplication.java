52e43406bd25ee86c82b4665207a23d3ff5587ce
Kiran H T <Kiran H T@DESKTOP-FNO72TO>
2020-02-25 21:40:14 +0530

    Back end initial commit

---
+++ BackEnd/homemadefood/src/main/java/com/homefood/homemadefood/HomemadefoodApplication.java
@@ -0,0 +1,13 @@
+package com.homefood.homemadefood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomemadefoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomemadefoodApplication.class, args);
+	}
+
+}
---
+++ BackEnd/homemadefood/src/test/java/com/homefood/homemadefood/HomemadefoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.homefood.homemadefood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomemadefoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
