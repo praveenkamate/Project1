e60a27d4f8d86d6e7b233340e51068b78aeeedc6
Kiran H T <kiranht14@gmail.com>
2020-03-14 21:34:34 +0530

    angular form created for register.

--- src/app/Components/sign-up/sign-up.component.ts
+++ src/app/Components/sign-up/sign-up.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'sign-up',
@@ -9,7 +10,21 @@ export class SignUpComponent implements OnInit {
 
   constructor() { }
 
+  form = new FormGroup({
+    userid: new FormControl(''),
+    password: new FormControl(''),
+    name: new FormControl(''),
+    gender: new FormControl(''),
+    phone: new FormControl(''),
+    dob: new FormControl('')
+  })
+
   ngOnInit(): void {
+  }
+
+  onSubmit() {
+    console.log("HI")
+    console.log(this.form.value);
   }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule} from '@angular/forms'
 
 import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { from } from 'rxjs';
 // import { SignUpComponent } from './Components/sign-up/sign-up.component';
 // import { LoginComponent } from './Components/login/login.component';
 
@@ -15,7 +17,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
